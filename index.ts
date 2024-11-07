59cbef8ac3c2372a30ba69718f3ee7bf1d6dce55
Kévin <voisine.kevin2@gmail.com>
2024-11-07 20:33:32 +0100

    [✨ feat] ajout, edition et suppression de donnée via l'ORM

--- index.ts
+++ index.ts
@@ -33,8 +33,46 @@ const User = sequelize.define(
 );
 
 User.sync({ alter: true })
-    .then((data) => {
+    .then(() => {
         console.log("Table and model synced successfuly !");
+
+        // la création d'un utilisateur est une opération asynchrone
+        // on peut donc utiliser les méthodes .then() et .catch()
+        // pour gérer la réponse de l'opération
+        // on a deux méthodes possibles :
+        // - la méthode build()
+        // - la méthode create()
+
+        // Création d'un utilisateur (méthode d'instance)
+        const user = User.build({ username: "Titouan", email: "titouan@gmail.com", password: "azerty" });
+        user.username = "Titou"
+        user.save();
+
+        // Affichage de l'utilisateur créé
+        console.log(user.toJSON());
+
+        // Création d'un utilisateur (méthode statique)
+        return User.create({
+            username: "Ji Tan",
+            email: "jitan@gmail.com",
+            password: "azerty",
+        });
+    }).then((data) => {
+        // Update de l'utilisateur créé
+        console.log(data.toJSON());
+        data.username = "Abdel";
+        data.email = "abdel@gmail.com";
+        // Enregistrement de l'utilisateur modifié
+        data.save();
+        // Affichage de l'utilisateur modifié
+        console.log(data.toJSON());
+
+        // suppression de l'utilisateur créé
+        data.destroy();
+
+        // suppression de l'utilisateur Titou
+        // En général prendre l'ID de l'utilisateur pas le nom
+        return User.destroy({ where: { username: 'Titou' } });
     })
     .catch(() => {
         console.log("Error while syncing the table and model");
