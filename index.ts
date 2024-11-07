bb34ebecbb65e9abf78a398c9a68131c3d802473
Kévin <voisine.kevin2@gmail.com>
2024-11-07 15:48:33 +0100

    [✨ feat] Première definition de table

--- index.ts
+++ index.ts
@@ -1,16 +1,36 @@
 // Apple des dépendances dans le projet
-const Sequelize = require('sequelize');
-const sqlite = require('sqlite3');
+const Sequelize = require("sequelize");
+const sqlite = require("sqlite3");
 
 const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: 'database.sqlite'
-  });
+    dialect: "sqlite",
+    storage: "database.sqlite",
+});
 
-async function loginToBDD(){
-    await sequelize.authenticate();
-    console.log('Connction successful');
-}
+const User = sequelize.define("user", {
+    id: {
+        type: Sequelize.DataTypes.INTEGER.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    username: {
+        type: Sequelize.DataTypes.STRING(96),
+        allowNull: false,
+        defaultValue: "Titouan",
+    },
+    email: {
+        type: Sequelize.DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+    },
+    password: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+    },
+});
 
-loginToBDD();
-console.log('Étape suivante ?');
+User.sync().then((data) => {
+    console.log('Table and model synced successfuly !')
+}).catch(() => {
+    console.log('Error while syncing the table and model')
+})
