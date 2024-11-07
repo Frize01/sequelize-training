af952212a148b140ee3902d1420dfb8156661b48
Kévin <voisine.kevin2@gmail.com>
2024-11-07 16:43:33 +0100

    [✨ feat] Ajouts des options

--- index.ts
+++ index.ts
@@ -7,30 +7,34 @@ const sequelize = new Sequelize({
     storage: "database.sqlite",
 });
 
-const User = sequelize.define("user", {
-    id: {
-        type: Sequelize.DataTypes.INTEGER.UNSIGNED,
-        autoIncrement: true,
-        primaryKey: true,
+const User = sequelize.define(
+    "user",
+    {
+        username: {
+            type: Sequelize.DataTypes.STRING(96),
+            allowNull: false,
+            defaultValue: "Titouan",
+        },
+        email: {
+            type: Sequelize.DataTypes.STRING,
+            unique: true,
+            allowNull: false,
+        },
+        password: {
+            type: Sequelize.DataTypes.STRING,
+            allowNull: false,
+        },
     },
-    username: {
-        type: Sequelize.DataTypes.STRING(96),
-        allowNull: false,
-        defaultValue: "Titouan",
-    },
-    email: {
-        type: Sequelize.DataTypes.STRING,
-        unique: true,
-        allowNull: false,
-    },
-    password: {
-        type: Sequelize.DataTypes.STRING,
-        allowNull: false,
-    },
-});
+    {
+        // freezeTableName: true,
+        // timestamps: false
+    }
+);
 
-User.sync().then((data) => {
-    console.log('Table and model synced successfuly !')
-}).catch(() => {
-    console.log('Error while syncing the table and model')
-})
+User.sync({ alter: true })
+    .then((data) => {
+        console.log("Table and model synced successfuly !");
+    })
+    .catch(() => {
+        console.log("Error while syncing the table and model");
+    });
