2606f299d8bfe616def4a1ad4a2a39837d52ab48
Frize <voisine.kevin2@gmail.com>
2024-11-28 11:02:30 +0100

    [🪄 reset] Project structure

--- server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app');
-const config = require('./config/app.config');
-
-app.listen(config.port, () => {
-  console.log(`Server is running on http://localhost:${config.port}`);
-});
